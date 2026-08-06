3fef261afe56cc76c8c1240968a791ede3862dbe
ytrytio <ghostersh.yt@gmail.com>
2026-08-06 17:44:29 +0200

    hotfix: listed all permissions in the get_permissions() function.

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -9,13 +9,21 @@ from ..config import GROUP
 def get_permissions(yet: bool):
     return ChatPermissions(
         can_send_messages=yet,
+        can_send_audios=yet,
+        can_send_photos=yet,
+        can_send_videos=yet,
         can_send_media_messages=yet,
+        can_send_video_notes=yet,
+        can_send_voice_notes=yet,
         can_send_stickers=yet,
         can_send_animations=yet,
         can_send_games=yet,
         can_use_inline_bots=yet,
         can_add_web_page_previews=yet,
         can_send_polls=yet,
+        can_send_other_messages=yet,
+        can_react_to_messages=yet,
+        can_send_documents=yet,
         can_change_info=False,
         can_invite_users=False,
         can_pin_messages=False
